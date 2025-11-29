4d709ec8f649e27256c6ab5036e75851639ccdc2
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-11-29 23:36:58 +0530

    understanding variables

---
+++ 01_basics/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main(){ 
+
+    int age = 18;           // 4 bytes
+    float price = 10.50;    // 4 bytes
+    char letter = 'A';      // 1 bytes
+    bool isEven = true;     // 1 bytes
+    double = 100.899;       // 8 bytes
+
+    return 0;
+}
