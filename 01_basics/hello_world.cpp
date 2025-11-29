ec1cc807e089b251053e1ef58c075155e3135618
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-11-29 23:27:49 +0530

    first cpp code

---
+++ 01_basics/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Telling the compiler to add these files/library
+using namespace std; // Use std as defualt
+
+int main(){ // function
+
+    cout << "hello, World!\n"; // output
+    return 0;
+}
