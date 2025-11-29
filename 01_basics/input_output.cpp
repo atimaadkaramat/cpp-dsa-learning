2d550f24f2ea630fefdb9fae0d5eebd5c6273859
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-11-29 23:45:30 +0530

    Add example for input/output

---
+++ 01_basics/input_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+int main(){ 
+
+    int age;
+
+    cout << "What is your age: "; // Output
+    cin >> age; // Input
+
+    cout << "My age is " << age; // Output variable
+
+    return 0;
+}
