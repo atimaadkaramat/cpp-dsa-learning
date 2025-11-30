3673bd6e41ae5fe8311935e0178419f71d510f21
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-11-30 19:26:05 +0530

    add example code

---
+++ 03_arrays_and_strings/arrays_basic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5];
+
+
+    // Array input
+    for (int i = 0; i < 5; i++)
+    {
+
+        cout << "Add number in array: ";
+        cin >> arr[i];
+    
+    }
+
+    cout << endl;
+
+    // Array output
+    cout << "Display array elements: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
+    
+}
