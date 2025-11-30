64b85d8b87b01eb49fe31d1af9aeb48cafcd81fa
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-11-30 19:41:35 +0530

    add example code

---
+++ 03_arrays_and_strings/string_and_char_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char str[50];
+
+    // String input
+    cout << "input your name: ";
+    cin >> str;
+
+    // String output
+    cout << "hello, " << str;
+    
+    return 0;
+}
