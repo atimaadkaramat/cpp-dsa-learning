43c03f0d631a67574e7a765da54cfafdc7debf23
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-12-04 20:43:38 +0530

    added code based of data type modifiers

---
+++ 01_basics/data_type_modifier.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << sizeof(long int) << endl;
+
+    cout << sizeof(short int) << endl;
+
+    cout << sizeof(long long) << endl;
+
+    cout << sizeof(signed int) << endl; // stores both -ve and +ve numbers
+
+    cout << sizeof(unsigned int) << endl; // stores only +ve numbers
+}
