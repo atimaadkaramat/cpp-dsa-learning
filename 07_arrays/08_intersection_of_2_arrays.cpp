25f39d12747f8fcc390ef2249897944b36a911ab
Atimaad Karamat <atimaadkaramat@gmail.com>
2025-12-30 20:54:02 +0530

    add practice code

---
+++ 07_arrays/08_intersection_of_2_arrays.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void intersect(int arr1[], int arr2[], int size1, int size2){
+
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            if(arr1[i] == arr2[j])
+            {
+                cout << arr1[i] << " ";
+            }
+        }
+        
+    }
+    
+
+}
+
+
+int main(){
+
+    int arr1[] = {1,2,3,4};
+    int size1 = 4;
+    int arr2[] = {3,4,5,6};
+    int size2 = 4;
+
+    intersect(arr1,arr2,size1,size2);
+    return 0;
+}
