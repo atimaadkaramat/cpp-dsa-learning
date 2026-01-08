bb94af588b61ba92e82e68cc50929167f822c6fb
Atimaad Karamat <atimaadkaramat@gmail.com>
2026-01-08 22:11:31 +0530

    add practice code

---
+++ leetcode_problems/33. Search in Rotated Sorted Array/01_solution.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    int st = 0, end = nums.size() - 1;
+
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        if (nums[st] <= nums[mid])
+        { // left sorted
+            if (nums[st] <= target && target <= nums[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                st = mid + 1;
+            }
+        }
+        else
+        { // right sorted
+            if (nums[mid] <= target && target <= nums[end])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] <= target && target <= nums[end])
+                {
+                    st = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> vec = {6, 7, 0, 1, 2, 3, 4, 5};
+    int target = 7;
+
+    cout << search(vec, target);
+
+    return 0;
+}
