0c572dd8f74fc7fe0f91bb33241749ca34f57683
Atimaad Karamat <atimaadkaramat@gmail.com>
2026-02-16 21:55:08 +0530

    add vector iterator code

---
+++ 12_STL/01_vector/05_iterator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(vector<int>& vec){
+    for(int val : vec) {
+        cout << val << " ";
+    }
+
+    cout << endl;
+
+}
+
+int main(){
+
+    vector<int> vec = {1,2,3,4,5};
+
+    cout << "vector.begin() = " << *(vec.begin()) << endl;
+    cout << "vector.end() = " << *(vec.end()) << endl; // garbage value
+
+    for(vector<int>::iterator it = vec.begin(); it != vec.end(); it++){
+        cout << *(it) << " ";
+    }
+    cout << endl;
+
+    for(auto it2 = vec.rbegin(); it2 != vec.rend(); it2++){
+        cout << *(it2) << " ";
+    }
+
+    return 0;
+}
